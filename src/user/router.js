6882e4854ec5671e6646b4e22725b369ffac3693
Basar Buyukkahraman <basarbk@yahoo.com>
2024-06-28 22:01:50 +0300

    0402-validation

--- src/user/router.js
+++ src/user/router.js
@@ -1,11 +1,24 @@
 import { Router } from "express";
 import { save } from "./service.js";
+import { body, validationResult } from "express-validator";
 
 const userRouter = new Router();
 
-userRouter.post("/api/users", async (req, res) => {
-  await save(req.body);
-  res.send({ message: "Success" });
-});
+userRouter.post(
+  "/api/users",
+  body("email").isEmail().withMessage("Invalid email"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => {
+        validationErrors[error.path] = error.msg;
+      });
+      return res.status(400).send({ validationErrors });
+    }
+    await save(req.body);
+    res.send({ message: "Success" });
+  },
+);
 
 export default userRouter;

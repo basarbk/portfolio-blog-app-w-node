da0c6e607580caa6a4e455ae841ff1cdd732f4a6
Basar Buyukkahraman <basarbk@yahoo.com>
2024-06-30 23:21:47 +0300

    0701-seed

---
+++ database/seeders/20240630200244-init.cjs
@@ -0,0 +1,67 @@
+const { LoremIpsum } = require("lorem-ipsum");
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    min: 4,
+    max: 8,
+  },
+  wordsPerSentence: {
+    min: 4,
+    max: 8,
+  },
+});
+
+const generateSlug = (value) => {
+  return encodeURIComponent(value.toLowerCase().replaceAll(" ", "-"));
+};
+
+("use strict");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+    const users = [];
+    for (let i = 1; i <= 10; i++) {
+      users.push({
+        name: `user${i}`,
+        handle: `user${i}`,
+        email: `user${i}@mail.com`,
+      });
+    }
+    await queryInterface.bulkInsert("users", users);
+
+    const [savedUsers] = await queryInterface.sequelize.query(
+      "SELECT * from users",
+    );
+
+    const tokens = [];
+    const articles = [];
+
+    for (const user of savedUsers) {
+      tokens.push({
+        token: "token-" + user.name,
+        userId: user.id,
+      });
+      for (let i = 0; i < 5; i++) {
+        const article = {};
+        lorem.format = "plain";
+        article.title = lorem.generateWords(5);
+        article.slug = generateSlug(article.title);
+        lorem.format = "html";
+        article.content = lorem.formatString(lorem.generateParagraphs(2));
+        article.userId = user.id;
+        article.published = i < 4;
+        article.published_at = i < 4 ? new Date() : null;
+        articles.push(article);
+      }
+    }
+
+    await queryInterface.bulkInsert("tokens", tokens);
+    await queryInterface.bulkInsert("articles", articles);
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};

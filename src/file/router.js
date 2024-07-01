8730faf4b63ec0d430cd8568e25b4a984273b44f
Basar Buyukkahraman <basarbk@yahoo.com>
2024-07-01 22:31:27 +0300

    0802-validation

--- src/file/router.js
+++ src/file/router.js
@@ -1,14 +1,33 @@
 import { Router } from "express";
 import multer from "multer";
+import ValidationException from "../error/ValidationException.js";
 
 const fileRouter = new Router();
 
+const ONE_MB = 1024 * 1024;
+
 const upload = multer({
   dest: "./upload",
+  limits: { fileSize: ONE_MB },
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+      return callback(null, true);
+    }
+    callback(new Error("Unsupported file type"), false);
+  },
 }).single("file");
 
-fileRouter.post("/api/file/upload", upload, (req, res) => {
-  res.status(201).send({ filename: req.file.filename });
+fileRouter.post("/api/file/upload", (req, res, next) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return next(
+        new ValidationException({
+          file: "Uploaded file can only be jpeg or png and must be less than 1MB",
+        }),
+      );
+    }
+    res.status(201).send({ filename: req.file.filename });
+  });
 });
 
 export default fileRouter;
